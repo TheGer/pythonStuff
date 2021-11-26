b4cc85e7a9b3df445a5de27ede19a44fc3d2692b
Gerard Said <Gerard.Said@mcast.edu.mt>
2021-11-26 11:29:45 +0100

    lesson

---
+++ examples/RGB selector.py
@@ -0,0 +1,46 @@
+# import the necessary packages
+
+
+import cv2
+import numpy as np
+
+def donothing(x):
+    pass
+
+img = cv2.imread('lena.jpg')
+im_mod = img.copy()
+
+cv2.namedWindow("Color Manipulator1")
+#label of trackbar, window title, starting value, ending value, method that is called on change
+cv2.createTrackbar('RED', 'Color Manipulator1', 1, 1, donothing)
+cv2.createTrackbar('GREEN', 'Color Manipulator1', 1, 1, donothing)
+cv2.createTrackbar('BLUE', 'Color Manipulator1', 1, 1, donothing)
+
+
+
+
+while True:
+
+    posB = cv2.getTrackbarPos("BLUE", "Color Manipulator")
+    posG = cv2.getTrackbarPos("GREEN", "Color Manipulator")
+    posR = cv2.getTrackbarPos("RED", "Color Manipulator")
+
+    if posR == 0 :
+        im_mod[:, :, 2] = 0
+    else : im_mod[:, :, 2] = img[:,:,2]
+
+    if posG == 0:
+        im_mod[:, :, 1] = 0
+    else:
+        im_mod[:, :, 1] = img[:, :, 1]
+
+    if posB == 0 :
+        im_mod[:, :, 0] = 0
+    else : im_mod[:, :, 0] = img[:,:,0]
+
+    cv2.imshow("Output ", im_mod)
+
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
